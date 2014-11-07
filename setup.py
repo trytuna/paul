ea5f017ff2bd106be08834ffcdc755499d80492e
Timo Schrappe <schrappe.t@thirdman.de>
2014-11-08 00:18:36 +0100

    * [FIX] PEP8 compatible

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@
 from distutils.core import setup
 import os
 
-setup(  name='Paul',
-        version='1.0.0',
-        url='https://github.com/methanol/paul',
-        description='Paul is a commandline tool to send push notifications via prowl to your smartphone',
-        long_description=open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r').read(),
-        author='Timo Schrappe',
-        author_email='schrappe.t@thirdman.de',
-        py_modules=['paul'],
-        scripts=['bin/paul'],
-        license='MIT'
-)
+desc = open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r').read()
+
+setup(name='Paul',
+      version='1.0.0',
+      url='https://github.com/methanol/paul',
+      description='Paul is a commandline tool to send push notifications via '
+                  'prowl to your smartphone',
+      long_description=desc,
+      author='Timo Schrappe',
+      author_email='schrappe.t@thirdman.de',
+      py_modules=['paul'],
+      scripts=['bin/paul'],
+      license='MIT')
